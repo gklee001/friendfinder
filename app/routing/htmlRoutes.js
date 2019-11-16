15ae9ef85fd4b4553a1240216c611471b7b7f17d
Kneeslapper <gloriaklee8@gmail.com>
2019-11-16 00:50:23 -0800

    need to continue serialize array

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,9 +1,3 @@
-// Your `htmlRoutes.js` file should include two routes:
-
-// * A GET Route to `/survey` which should display the survey page.
-// * A default, catch-all route that leads to `home.html` which displays the home page.
-
-
 // Dependencies 
 var express = require("express");
 var path = require("path");
